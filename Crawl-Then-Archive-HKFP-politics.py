152bd31de463b0df9b0e3298c6893ecb22b53df1
FireInsight <rauhalaeino@gmail.com>
2021-06-28 14:56:48 +0300

    Made this too fast using multiprocessing

--- Crawl-Then-Archive-HKFP-politics.py
+++ Crawl-Then-Archive-HKFP-politics.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib.request
 from archivenow import archivenow
+import multiprocessing as mp
 
-for i in range(2, 1187):
-    request = urllib.request.Request("https://hongkongfp.com/category/topics/politics-protest/page/"+i)
+def archivePage(pagenum):
+    request = urllib.request.Request(f"https://hongkongfp.com/category/topics/politics-protest/page/{pagenum}/")
     request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0')
 
     html_page = urllib.request.urlopen(request)
@@ -11,3 +12,10 @@ for i in range(2, 1187):
     for link in soup.find_all('a'):
         oglink = str(link.get('href'))
         archivenow.push(oglink,"ia")
+
+def main():
+    pool = mp.Pool(mp.cpu_count())
+    pool.map(archivePage, range(1, 1187))
+
+if __name__ == "__main__":
+    main()
